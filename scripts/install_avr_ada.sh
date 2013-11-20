1e65a0ed680307bbb59ab788f855e2d6888009d1
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2013-11-20 10:11:20 +0100

    Added the last lines to the script file. Still only prints out the lines.

--- scripts/install_avr_ada.sh
+++ scripts/install_avr_ada.sh
@@ -49,7 +49,7 @@ echo "#####################################################"
 echo ""
 echo . ./native_build_env.sh
 echo wget ftp://ftp.mirrorservice.org/sites/sourceware.org/pub/gcc/releases/gcc-4.7.0/gcc-4.7.0.tar.gz
-# CHANGE: Added v paramter for verbose output
+# CHANGE: Added v parameter for verbose output
 echo tar zvxf gcc-4.7.0.tar.gz
 echo mkdir gcc-obj-470
 echo cd gcc-obj-470
@@ -89,4 +89,79 @@ echo ""
 
 echo sudo chmod +x /opt/gnat-4.7/bin/gcc-4.7 /opt/gnat-4.7/bin/g++-4.7 \
     /opt/gnat-4.7/bin/c++-4.7 /opt/gnat-4.7/bin/cpp-4.7
+
+echo ""
+echo "#####################################################"
+echo "# Install avr-binutils"
+echo "#####################################################"
+echo ""
+
+export CC="gcc-4.7" CXX="c++-4.7" CPP="cpp-4.7"
+echo . ./cross_build_env.sh
+echo wget http://ftp.uni-kl.de/pub/gnu/binutils/binutils-2.20.1a.tar.bz2
+# CHANGE: Added v parameter for verbose output
+echo tar jvxf binutils-2.20.1a.tar.bz2
+echo git clone git://git.code.sf.net/p/avr-ada/code avr-ada-code
+echo cd binutils-2.20.1
+echo 'for a in ../avr-ada-code/patches/binutils/2.20.1/*.patch;do patch -p0 < $a || fail "avr-binutils: patch $a" ; done'
+echo cd ..
+echo mkdir binutils-obj
+echo cd binutils-obj
+echo ../binutils-2.20.1/configure --target=avr --program-prefix=avr- --disable-shared --disable-nls --enable-commonbfdlib=no --disable-werror --prefix=$DEST_GNAT_CROSS
+echo make
+echo sudo su -c ". ../cross_build_env.sh; make install"
+echo cd ..
+
+echo ""
+echo "#####################################################"
+echo "# Install gnat-4.7 Cross compile for avr"
+echo "#####################################################"
+echo ""
+
+echo . ./native_build_env.sh
+echo . ./cross_build_env.sh
+echo mv gcc-4.7.0 gcc-4.7.0-native
+# CHANGE: Added v parameter for verbose output
+echo tar zvxf gcc-4.7.0.tar.gz
+echo cd gcc-4.7.0
+echo 'for a in ../avr-ada-code/patches/gcc/4.7.0/*.patch;do patch -p0 < $a;done'
+echo cd ..
+echo mkdir avr-gcc-obj
+echo cd avr-gcc-obj
+echo ../gcc-4.7.0/configure --target=avr --program-prefix=avr- --disable-shared --disable-nls --disable-libssp --with-system-zlib --disable-libada --enable-languages=ada,c,c++ --enable-cpp --with-dwarf2 --enable-version-specific-runtime-libs --prefix=$DEST_GNAT_CROSS
+echo sed -i -e 's/$(STAMP)/echo timestamp >/' ../gcc-4.7.0/gcc/config/avr/t-avr
+echo make
+echo sudo su -c ". ../native_build_env.sh; . ../cross_build_env.sh; make install"
+echo cd ..
+
+echo ""
+echo "#####################################################"
+echo "# Install cross avr-libc"
+echo "#####################################################"
+echo ""
+echo . ./cross_build_env.sh
+echo unset CC ; unset CXX ; unset CPP
+echo wget http://download.savannah.gnu.org/releases/avr-libc/avr-libc-1.8.0.tar.bz2
+# CHANGE: Added v parameter for verbose output
+echo tar jvxf avr-libc-1.8.0.tar.bz2
+echo cd avr-libc-1.8.0
+echo ./configure --host=avr --prefix=$DEST_GNAT_CROSS
+echo make
+echo sudo su -c ". ./cross_build_env.sh; make install"
+echo cd ..
+
+echo ""
+echo "#####################################################"
+echo "# Install AVR-Ada"
+echo "#####################################################"
+echo ""
+echo cd avr-ada-code
+echo ./configure
+echo make
+echo sudo su -c ". ../native_build_env.sh ; . ../cross_build_env.sh; make install_rts"
+echo cd avr/avr_lib
+echo sed -i -e 's/stamp-libs: $(thread_libs)/stamp-libs:/' Makefile
+echo make
+echo cd ../..
+echo sudo su -c ". ../cross_build_env.sh; make install_libs"
 
