164bd6173a3dc9a0dac1a4a1146560871560619f
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2014-01-04 13:16:34 +0100

    Temporary commit, testing how the different sensor fusion tasks work together.

--- src/software/sensor_fusion/TCP/src/sensor_fusion-tcp.adb
+++ src/software/sensor_fusion/TCP/src/sensor_fusion-tcp.adb
@@ -1,20 +1,30 @@
+with Ada.Text_IO;
+
 package body Sensor_Fusion.TCP is
+
+   ------------------
+   -- TCP_RESOURCE --
+   ------------------
 
    protected body TCP_Resource is
       procedure Send(xCANMessage : in Sensor_Fusion.Shared_Types.CAN_Message) is
       begin
-         --Ada.Text_IO.Put_Line("TCP_Resource: Send called."); -- for testing
-         null;
+         Ada.Text_IO.Put_Line("TCP_Resource: Send called."); -- for testing
+         -- null;
       end Send;
 
       procedure Receive(xCANMessage : out Sensor_Fusion.Shared_Types.CAN_Message; bMessageReceived : out boolean) is
          xNewCANMessage : Sensor_Fusion.Shared_Types.CAN_Message;
       begin
-         --Ada.Text_IO.Put_Line("TCP_Resource: Receive called."); -- for testing
+         Ada.Text_IO.Put_Line("TCP_Resource: Receive called."); -- for testing
          xCANMessage := xNewCANMessage;
          bMessageReceived := true;
       end Receive;
    end TCP_Resource;
+
+   -----------------
+   -- TASK_TCP_IN --
+   -----------------
 
    task body TASK_TCP_IN is
       xCANMessageFromShore : Sensor_Fusion.Shared_Types.CAN_Message;
@@ -22,16 +32,22 @@ package body Sensor_Fusion.TCP is
    begin
       loop
 
+         Ada.Text_IO.Put_Line("TCP_IN: Calling Receive."); -- for testing
          TCP_Resource.Receive(xCANMessage      => xCANMessageFromShore,
                               bMessageReceived => bMessageReceived);
 
          if bMessageReceived then
+            Ada.Text_IO.Put_Line("TCP_IN: Adding new CAN message to list."); -- for testing
             Sensor_Fusion.Shared_Types.xCANSimulatedMessageList.Add(xCANMessage => xCANMessageFromShore);
          end if;
 
-         --delay 0.5; -- for testing
+         delay 0.5; -- for testing
       end loop;
    end TASK_TCP_IN;
+
+   ------------------
+   -- TASK_TCP_OUT --
+   ------------------
 
    task body TASK_TCP_OUT is
       xCANMessageToShore : Sensor_Fusion.Shared_Types.CAN_Message;
@@ -39,7 +55,7 @@ package body Sensor_Fusion.TCP is
       loop
 
          if Sensor_Fusion.Shared_Types.xCANOutMessageList.iCount > 0 then
-
+            Ada.Text_IO.Put_Line("TCP_OUT: xCANOutMessageList has items in it."); -- for testing
             Sensor_Fusion.Shared_Types.xCANOutMessageList.Remove(xCANMessage => xCANMessageToShore);
             TCP_Resource.Send(xCANMessage => xCANMessageToShore);
 
@@ -48,12 +64,13 @@ package body Sensor_Fusion.TCP is
 
          if Sensor_Fusion.Shared_Types.xCANInMessageList.iCount > 0 then
 
+            Ada.Text_IO.Put_Line("TCP_OUT: xCANInMessageList has items in it."); -- for testing
             Sensor_Fusion.Shared_Types.xCANInMessageList.Remove(xCANMessage => xCANMessageToShore);
             TCP_Resource.Send(xCANMessage => xCANMessageToShore);
 
          end if;
 
-         --delay 0.5; -- for testing
+         delay 0.5; -- for testing
       end loop;
    end TASK_TCP_OUT;
 
